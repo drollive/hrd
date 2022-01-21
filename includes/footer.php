c4d79b8d10226211e5f2169622172f3a42067fdd
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-01-21 21:33:09 +0800

    hrd update

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+
+    <script src="asset/js/jquery3.min.js"></script>
+    <script src="asset/js/bootstrap5.bundle.min.js"></script>
+    <script src="asset/js/scripts.js"></script>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
