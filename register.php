3faff30c7c98f03093a68bbdfd169e2c144f4533
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-01-22 10:27:46 +0800

    hrd update

---
+++ register.php
@@ -0,0 +1,51 @@
+<?php
+
+include("includes/header.php");
+include("includes/navbar.php");
+?>
+
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Register</h4>
+                    </div>
+
+                    <div class="card-body">
+
+                        <div class="form-group mb-3">
+                            <label>First Name</label>
+                            <input type="text" placeholder="Enter First Name" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label>Last Name</label>
+                            <input type="text" placeholder="Enter Last Name" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label>Email ID</label>
+                            <input type="email" placeholder="Enter Email Address" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label>Password</label>
+                            <input type="password" placeholder="Enter Password" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+
+                    </div> 
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+
+include("includes/footer.php");
+
+?>
